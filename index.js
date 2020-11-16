5ecdb8e62e4194f2e2b45a8fc70b645cda81e61c
metrio <dlima.nyit@gmail.com>
2020-11-15 21:31:24 -0500

    commit

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ function handleClick(e) {
 /* Write your implementation of greet() */
 
 function greet(timeStr){
-const hour = parseInt(timeStr, 10);
-if (hour < 12) return "Good Morning"
-if (hour > 17) return "Good Evening"
-return "Good Afternoon"
+  const hour = parseInt(timeStr, 10);
+    if (hour < 12) return "Good Morning"
+    if (hour > 17) return "Good Evening"
+  return "Good Afternoon"
 }
 /* Write your implementation of displayMessage() */
 
